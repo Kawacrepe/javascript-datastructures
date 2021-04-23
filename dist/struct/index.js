fccd1087b73e4c669998defa63fd2b2d0773eb3d
Kawacrepe <vincent.dhennin@viacesi.fr>
2021-04-23 12:42:21 +0200

    remove unnecessary directory

--- dist/struct/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-const helloWorld_1 = require("./helloWorld");
-exports.helloWorld = helloWorld_1.default;
