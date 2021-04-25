b1922b817c1a1737535b63f24fbd7aa06d83140f
Kawacrepe <vincent.dhennin@viacesi.fr>
2021-04-25 22:13:48 +0200

    chore: add devdep

--- src/struct/linkedList/index.ts
+++ src/struct/linkedList/index.ts
@@ -1,43 +1,35 @@
 interface IListNode {
-  data: unknown,
-  next: IListNode
-}
-class LinkedList {
-  head: IListNode
-
-  constructor(head = null) {
-    this.head = head;
-  }
-
-  private getSize () {
-    let count = 0;
-    let node = this.head;
-    while (node) {
-      count++;
-      node = node.next;
-    }
-    return count;
-  }
-
-  private clear (): void {
-    this.head = null;
-  }
-
-  private getLast () {
-    let lastNode = this.head;
-    if (!lastNode) { return null }
-    while (lastNode.next) {
-      lastNode = lastNode.next;
-    }
-    return lastNode;
-  }
-
-  private getFirst () {
-    return this.head;
-  }
+  data: unknown;
+  next: IListNode;
 }
 
-export {
-  IListNode,
-  LinkedList
-};
+function getSize() {
+  let count = 0;
+  let node = this.head;
+  while (node) {
+    count++;
+    node = node.next;
+  }
+  return count;
+}
+
+function clear(): void {
+  this.head = null;
+}
+
+function getLast() {
+  let lastNode = this.head;
+  if (!lastNode) {
+    return null;
+  }
+  while (lastNode.next) {
+    lastNode = lastNode.next;
+  }
+  return lastNode;
+}
+
+function getFirst() {
+  return this.head;
+}
+
+export { IListNode };
